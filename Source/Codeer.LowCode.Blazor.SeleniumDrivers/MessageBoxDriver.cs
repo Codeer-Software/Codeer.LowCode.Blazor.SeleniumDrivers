ee6d044b85ca75e194e69de743c601f2d4a7ca6f
yutaka <shigure@refy.net>
2024-10-01 03:14:13 +0900

    Add messagebox selenium driver

---
+++ Source/Codeer.LowCode.Blazor.SeleniumDrivers/MessageBoxDriver.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using Selenium.StandardControls.PageObjectUtility;
+using Selenium.StandardControls;
+using Selenium.StandardControls.TestAssistant.GeneratorToolKit;
+
+namespace Codeer.LowCode.Blazor.SeleniumDrivers
+{
+    public class MessageBoxDriver : ComponentBase
+    {
+        public IWebElement Body => ByCssSelector(".modal-body").Wait().Find();
+        public ItemsControlDriver<ButtonDriver> Buttons => ByCssSelector(".modal-footer").Wait().Find<ItemsControlDriver<ButtonDriver>>();
+        public ButtonDriver CloseButton => ByCssSelector("button.btn-close").Wait();
+        public MessageBoxDriver(IWebElement element) : base(element) { }
+        public static implicit operator MessageBoxDriver(ElementFinder finder) => finder.Find<MessageBoxDriver>();
+    }
+
+    public static class MessageBoxExtension
+    {
+        [ComponentObjectIdentify]
+        public static MessageBoxDriver AttachMessageBox(this IWebDriver driver)
+            => new MappingBase(driver).ByCssSelector(".modal.show[data-system='message-box']").Wait();
+
+    }
+}
